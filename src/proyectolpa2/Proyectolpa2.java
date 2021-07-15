ce62890c5615e2712fa0f779c95960d23bda68e5
Harold <Harold@DESKTOP-AFGP76O>
2021-07-15 16:39:08 -0500

    Mensaje de hola mundo !

--- src/proyectolpa2/Proyectolpa2.java
+++ src/proyectolpa2/Proyectolpa2.java
@@ -15,6 +15,7 @@ public class Proyectolpa2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+     System.out.println("Hola Mundo");
         // TODO code application logic here
     }
     
